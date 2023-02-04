41f63cad1a80b18ee34032b77bd0b508d1663f3e
RMA-17 <rakamuhammadalhafidz12312@gmail.com>
2023-02-04 13:57:49 +0700

    Fixed last read by page bug

--- app/src/main/java/com/rmaproject/myqoran/utils/Queries.kt
+++ app/src/main/java/com/rmaproject/myqoran/utils/Queries.kt
@@ -19,7 +19,7 @@ object Queries {
 
     fun getReadByPageQuery(pageNumber: Int): SimpleSQLiteQuery {
         val simpleQuery =
-            StringBuilder().append("SELECT id, sora, jozz, aya_no, aya_text, aya_text_emlaey, translation_id, sora_name_ar, sora_name_en, footnotes_id, translation_en, footnotes_en, sora_descend_place FROM quran WHERE page = ")
+            StringBuilder().append("SELECT id, sora, jozz, aya_no, page,aya_text, aya_text_emlaey, translation_id, sora_name_ar, sora_name_en, footnotes_id, translation_en, footnotes_en, sora_descend_place FROM quran WHERE page = ")
         simpleQuery.append(pageNumber)
         return SimpleSQLiteQuery(simpleQuery.toString())
     }
